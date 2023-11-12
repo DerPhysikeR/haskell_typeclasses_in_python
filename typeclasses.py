9eeb57d451a1ea883e788c87752ff2f28bcfe868
Paul Reiter <reiter.paul@gmail.com>
2023-11-12 10:07:50 +0100

    Introduce MyList and make it an instance of Functor

--- typeclasses.py
+++ typeclasses.py
@@ -1,4 +1,4 @@
-from typing import Callable, Protocol, Self
+from typing import Callable, Iterator, Protocol, Self
 
 
 class Functor(Protocol):
@@ -19,7 +19,21 @@ class Maybe:
         return self.__class__(fun(self._value))
 
 
-# function that needs an instance of Functor
+class MyList:
+    def __init__(self, ls: list) -> None:
+        self._list = ls
+
+    def __repr__(self) -> str:
+        return f"MyList({repr(self._list)})"
+
+    def __iter__(self) -> Iterator:
+        return iter(self._list)
+
+    def fmap(self, fun: Callable) -> Self:
+        return self.__class__(list(map(fun, self._list)))
+
+
+# function which requires an instance of Functor
 def square(arg: Functor) -> Functor:
     return arg.fmap(lambda x: x * x)
 
@@ -28,5 +42,10 @@ if __name__ == "__main__":
     # functor examples
     maybe_none = Maybe(None)
     maybe_just = Maybe(5)
-    print(square(maybe_none))
-    print(square(maybe_just))
+    print(square(maybe_none))  # Maybe(None)
+    print(square(maybe_just))  # Maybe(25)
+
+    empty_list = MyList([])
+    some_list = MyList([1, 2, 3])
+    print(square(empty_list))  # MyList([])
+    print(square(some_list))  # MyList([1, 4, 9])
